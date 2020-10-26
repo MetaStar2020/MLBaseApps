67f93fc09eeaf8e0b472d350b291413231bdbce5
Chantal-Deguire <5s5onscreen@gmail.com>
2020-10-26 14:29:28 -0400

    Directory Updates

---
+++ FaceDetector/FaceDetector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceDetector
+//
+//  Created by Chantal Deguire on 2020-10-26.
+//  Copyright © 2020 5S5. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
