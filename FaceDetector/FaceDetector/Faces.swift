8a082353c37f751fa946bf933de5986ee5e35050
Chantal-Deguire <5s5onscreen@gmail.com>
2020-12-23 16:17:29 -0500

    Feat: Added rectangles on detected faces.

--- FaceDetector/FaceDetector/Faces.swift
+++ FaceDetector/FaceDetector/Faces.swift
@@ -34,37 +34,40 @@ extension UIImage {
     }
 }
 
-/*
-extension UIImage.Orientation {
-    init(_ cgOrientation: UIImage.Orientation) {
-        switch cgOrientation {
-            case .up: self = .up
-            case .upMirrored: self = .upMirrored
-            case .down: self = .down
-            case .downMirrored: self = .downMirrored
-            case .left: self = .left
-            case .leftMirrored: self = .leftMirrored
-            case .right: self = .right
-            case .rightMirrored: self = .rightMirrored
-        @unknown default:
-            self = .up //Might have to change here?
+extension Collection where Element == VNFaceObservation {
+    
+    func drawnOn(_ image: UIImage) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(image.size, false, 1.0)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
         }
+        image.draw(in: CGRect(x: 0,
+                              y: 0,
+                              width: image.size.width,
+                              height: image.size.height))
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(0.01 * image.size.width)
+        
+        let transform = CGAffineTransform(scaleX: 1, y: -1)
+            .translatedBy(x: 0, y: -image.size.height)
+        
+        for observation in self {
+            let rect = observation.boundingBox
+            
+            let normalizedRect =
+            VNImageRectForNormalizedRect(rect,
+                                         Int(image.size.width),
+                                         Int(image.size.height))
+                .applying(transform)
+            
+            context.stroke(normalizedRect)
+        }
+        
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return result
     }
 }
-
-extension CGImagePropertyOrientation {
-    init(_ uiOrientation: UIImage.Orientation) {
-        switch uiOrientation {
-            case .up: self = .up
-            case .upMirrored: self = .upMirrored
-            case .down: self = .down
-            case .downMirrored: self = .downMirrored
-            case .left: self = .left
-            case .leftMirrored: self = .leftMirrored
-            case .right: self = .right
-            case .rightMirrored: self = .rightMirrored
-        @unknown default:
-            self = .up // What else should it be?
-        }
-    }
-}*/
