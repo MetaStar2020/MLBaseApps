6f8802fa7cbc218442104bd70c76ab7072a877b5
Chantal-Deguire <5s5onscreen@gmail.com>
2021-01-12 16:32:22 -0500

    Initial Commit 'ImageClassifier'

---
+++ ImageClassifier/ImageClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageClassifier
+//
+//  Created by Chantal Deguire on 2021-01-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
