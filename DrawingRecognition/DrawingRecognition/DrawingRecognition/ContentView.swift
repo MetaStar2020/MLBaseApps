272547f404800de67d1fa0f27f744f83a786ca09
Chantal-Deguire <5s5onscreen@gmail.com>
2023-01-23 16:41:05 -0500

    Initialize Drawing Recognition App

---
+++ DrawingRecognition/DrawingRecognition/DrawingRecognition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawingRecognition
+//
+//  Created by Chantal Deguire on 2023-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
