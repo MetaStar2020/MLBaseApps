dbed774e1580462e8e03d9180b14b8228f3df678
Chantal-Deguire <5s5onscreen@gmail.com>
2023-02-08 15:55:44 -0500

    Created Interface and connected to recognizer

--- SpeechRecognition/SpeechRecognition/SpeechRecognition/ContentView.swift
+++ SpeechRecognition/SpeechRecognition/SpeechRecognition/ContentView.swift
@@ -6,8 +6,33 @@
 //
 
 import SwiftUI
+import Speech
+import AVFoundation
+
+struct ButtonLabel: View {
+    private let title: String
+    private let background: Color
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(title)
+                .font(.title)
+                .bold()
+                .foregroundColor(.white)
+            Spacer()
+        }.padding().background(background).cornerRadius(10)
+    }
+    
+    init(_ title: String, background: Color) {
+        self.title = title
+        self.background = background
+    }
+}
 
 struct ContentView: View {
+    @State var recording: Bool = false
+    @State var speech: String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +41,33 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+    
+    private let recognizer: SpeechRecognizer
+    
+    init() {
+        guard let recognizer = SpeechRecognizer() else {
+            fatalError("Something went wrong...")
+        }
+        self.recognizer = recognizer
+    }
+    
+    private func startRecording() {
+        self.recording = true
+        self.speech = ""
+        
+        recognizer.startRecording { result in
+            if let text = result {
+                self.speech = text
+            } else {
+                self.stopRecording()
+            }
+        }
+    }
+    
+    private func stopRecording() {
+        self.recording = false
+        recognizer.stopRecording()
     }
 }
 
