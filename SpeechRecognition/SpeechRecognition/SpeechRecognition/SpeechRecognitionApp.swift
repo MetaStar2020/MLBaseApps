673aa5379f4fc1ebde6bed277b5097b81e92fbee
Chantal-Deguire <5s5onscreen@gmail.com>
2023-02-08 13:39:31 -0500

    Speech First Commit: Initializing Mini App

---
+++ SpeechRecognition/SpeechRecognition/SpeechRecognition/SpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionApp.swift
+//  SpeechRecognition
+//
+//  Created by Chantal Deguire on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
